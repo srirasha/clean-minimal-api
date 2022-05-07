11734f74dd5ce254a1dcc347db133a819c47f13b
Shahrukh Khan <sha.khan@outlook.fr>
2022-05-07 21:20:50 +0200

    Setup log

--- src/Web.Clean.Minimal.API/Program.cs
+++ src/Web.Clean.Minimal.API/Program.cs
@@ -1,10 +1,24 @@
 using Application;
 using Application.WeatherForecasts.Queries.GetWeatherForecast;
 using MediatR;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
 using Web.Clean.Minimal.API.Middleware;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration().Enrich.FromLogContext()
+                                      .Enrich.WithExceptionDetails()
+                                      .WriteTo.Console()
+                                      .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(builder.Configuration["Elastic:Uri"]))
+                                      {
+                                          AutoRegisterTemplate = true,
+                                          IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy}"
+                                      })
+                                      .CreateLogger();
+builder.Host.UseSerilog();
 builder.Services.AddApplication();
 
 builder.Services.AddEndpointsApiExplorer();
