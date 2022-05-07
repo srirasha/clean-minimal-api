23b55eb119bc8be7470d9cf344e81ff8d8e728dd
Shahrukh Khan <sha.khan@outlook.fr>
2022-05-07 22:40:06 +0200

    Clean Program.cs Create extensions

--- src/Web.Clean.Minimal.API/Program.cs
+++ src/Web.Clean.Minimal.API/Program.cs
@@ -2,39 +2,20 @@ using Application;
 using Application.WeatherForecasts.Queries.GetWeatherForecast;
 using MediatR;
 using Serilog;
-using Serilog.Exceptions;
-using Serilog.Sinks.Elasticsearch;
-using System.Reflection;
+using Web.Clean.Minimal.API.Extensions;
 using Web.Clean.Minimal.API.Middleware;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration().Enrich.FromLogContext()
-                                      .Enrich.WithExceptionDetails()
-                                      .WriteTo.Console()
-                                      .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(builder.Configuration["Elastic:Uri"]))
-                                      {
-                                          AutoRegisterTemplate = true,
-                                          IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy}"
-                                      })
-                                      .CreateLogger();
-builder.Host.UseSerilog();
-builder.Services.AddApplication();
+builder.AddSerilog();
+builder.AddSwagger();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
 
 WebApplication app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.EnableSwagger();
 
 app.MapGet("api/v1/weatherforecast/city/{city}", (IMediator mediator, string city, CancellationToken cancellationToken) =>
 {
