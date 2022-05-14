6ff729fdbe58489b8b4152fa3c90947ad87ecc64
Shahrukh Khan <sha.khan@outlook.fr>
2022-05-14 18:50:45 +0200

    Remove abstract mongo shits

--- src/Infrastructure/Persistence/Contexts/AssetsDb/Entities/BaseEntities/Configurations/DbSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Infrastructure.Persistence.Contexts.AssetsDb.Entities.BaseEntities.Configurations
-{
-    public class DbSettings : IDbSettings
-    {
-        public string DatabaseName { get; set; }
-        public string ConnectionString { get; set; }
-    }
-}
--- src/Infrastructure/Persistence/Contexts/AssetsDb/Entities/BaseEntities/Documents/BaseDocument.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Bson;
-
-namespace Infrastructure.Persistence.Contexts.AssetsDb.Entities.BaseEntities.Documents
-{
-    public class BaseDocument
-    {
-        public ObjectId Id { get; set; }
-    }
-}
